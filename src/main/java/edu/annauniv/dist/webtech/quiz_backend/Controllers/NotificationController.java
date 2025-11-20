a76d1cf3c81a1204d9266328b6d41e77a097617b
Rishi-Kumar <152687290+Rishi-kumar18@users.noreply.github.com>
2025-11-20 16:56:54 +0530

    commit

--- src/main/java/edu/annauniv/dist/webtech/quiz_backend/Controllers/NotificationController.java
+++ src/main/java/edu/annauniv/dist/webtech/quiz_backend/Controllers/NotificationController.java
@@ -4,12 +4,16 @@ import edu.annauniv.dist.webtech.quiz_backend.Views.NotificationView;
 import edu.annauniv.dist.webtech.quiz_backend.Views.email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/notifications")
 public class NotificationController {
+
+    private static final Logger log = LoggerFactory.getLogger(MarksController.class);
 
     @Autowired
     NotificationView notificationView;
@@ -18,11 +22,12 @@ public class NotificationController {
 
     @PostMapping("/send")
     public String sendNotification(@RequestBody Map<String, String> payload) {
-        System.out.println(payload.get("token"));
+        log.info("Request /notifications/send token=", payload.get("token"));
         String token = payload.get("token");
         String title = payload.get("title");
         String body = payload.get("body");
-        email.sendEmail("jeyabalan764@gmail.com","You have a new Test","Attend the Test without fail");
+        String semail = payload.get("email");
+        email.sendEmail("rk18102004@gmail.com","You have a new Test","Attend the Test without fail");
         return notificationView.sendPushNotification(token, title, body);
     }
 }
